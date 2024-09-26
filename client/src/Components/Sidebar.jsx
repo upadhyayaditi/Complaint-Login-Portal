f961adbf358aa871ad47f81428f545abf647c1fb
upadhyayaditi <au.aditi08@gmail.com>
2024-09-26 16:28:17 +0530

    Sidebar component added

---
+++ client/src/Components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className=' flex flex-col  ' >
+        <div>
+            <h2 className=' text-xl font-semibold ' >Complaint</h2>
+            <h4 className='mb-1 text-gray-400 ' >Complaint@gmail.com</h4>
+            <hr />
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Sidebar from '../Components/Sidebar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex h-[98vh] gap-4 '>
+      <div className=' w-1/6 border rounded-xl p-4 border-gray-400 '>
+        <Sidebar />
+      </div>
+      <div className=' w-5/6 border rounded-xl p-4 border-gray-400'>hello2</div>
+    </div>
   )
 }
 
