24d0c4260b90d6e30a1675ab7f7cdde0d319737d
upadhyayaditi <au.aditi08@gmail.com>
2024-10-03 14:10:49 +0530

    update navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='bg-white bg-opacity-75 flex items-center justify-between px-6 py-2 drop-shadow w-full fixed top-0 z-50'>
     <div className="flex-2"> 
-      <img src={logo} className="h-12 w-24 md:h-16 md:w-32 lg:h-14 lg:w-40" alt="logo" /> {/* Adjusted size for different screen sizes */}
+      <img src={logo} className="h-12 w-24 md:h-16 md:w-32 lg:h-12 lg:w-40" alt="logo" /> 
     </div>
     <div className="md:flex md:items-center md:space-x-4 ml-auto">
       <h2 className='hidden md:block text-2xl md:text-2xl lg:text-3xl font-bold text-black py-2 px-2 md:px-2' style={{ color: '#195c90' }}>
